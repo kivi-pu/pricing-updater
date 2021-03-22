8f66fd8c3d174c173870d0bd6eb3a4d6a5ccaaa7
Just Me <just806me@gmail.com>
2021-03-22 14:48:57 +0200

    added more details to error messages

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 	content, err := ioutil.ReadFile(os.Args[1])
 
 	if err != nil {
-		panic(fmt.Errorf("failed to read file content at path %s: %w", os.Args[1], err))
+		panic(fmt.Errorf("failed to read file content at path %s: %s\n%w", os.Args[1], err.Error(), err))
 	}
 
 	err = pushFile(content, "kivi-pu", "products", "products.xml", "updated products")
@@ -41,7 +41,7 @@ func pushFile(content []byte, owner, repo, path, message string) error {
 	file, _, _, err := client.Repositories.GetContents(ctx, owner, repo, path, nil)
 
 	if err != nil {
-		return fmt.Errorf("failed to get file from github: %w", err)
+		return fmt.Errorf("failed to get file from github: %s\n%w", err.Error(), err)
 	}
 
 	_, _, err = client.Repositories.UpdateFile(ctx, owner, repo, path, &github.RepositoryContentFileOptions{
@@ -49,7 +49,7 @@ func pushFile(content []byte, owner, repo, path, message string) error {
 	})
 
 	if err != nil {
-		return fmt.Errorf("failed to push file to github: %w", err)
+		return fmt.Errorf("failed to push file to github: %s\n%w", err.Error(), err)
 	}
 
 	return nil
